9aef5c51df1e529390feaeaff3f2349b9832391f
chris ren <rj532029887@live.com>
2016-07-29 11:59:33 +0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CmsTest.Startup))]
+namespace CmsTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
